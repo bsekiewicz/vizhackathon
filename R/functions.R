746b04f71666de9ea78dac6ae859c9aea6f8ad1c
Maciej Kasinski <maciejkasinski@gmail.com>
2019-09-26 16:45:24 +0200

    add district filter, new scoring

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,5 @@
+
+
 score <- function(data, params=list(), districts=NULL, wgs = wgs) {
   
   # data <- read_csv('data/preprocessing/places_with_wgs84.csv')
@@ -86,9 +88,9 @@ score_2 <- function(data, wgs, params=list(), districts=NULL) {
   for (t in types_in) {
     tmp <- data %>% filter(type==t) %>%
       group_by(wgs) %>% 
-      summarise(mean_rating = mean(rating, rm.na=TRUE)) %>%
+      summarise(mean_rating = mean(rating, na.rm=TRUE)) %>%
       arrange(desc(mean_rating))
-    tmp$mean_rating[is.na(tmp$mean_rating)] <- 0 #mean(tmp$mean_rating, rm.na=TRUE)
+    tmp$mean_rating[is.na(tmp$mean_rating)] <- 0 #mean(tmp$mean_rating, na.rm=TRUE)
     tmp <- left_join(output, tmp)
     tmp$mean_rating[is.na(tmp$mean_rating)] <- 0
     tmp$score <- tmp$score + tmp$mean_rating
@@ -97,9 +99,9 @@ score_2 <- function(data, wgs, params=list(), districts=NULL) {
   for (t in types_out) {
     tmp <- data %>% filter(type==t) %>%
       group_by(wgs) %>% 
-      summarise(mean_rating = 5-mean(rating, rm.na=TRUE)) %>%
+      summarise(mean_rating = 5-mean(rating, na.rm=TRUE)) %>%
       arrange(desc(mean_rating))
-    tmp$mean_rating[is.na(tmp$mean_rating)] <- 5 #mean(tmp$mean_rating, rm.na=TRUE)
+    tmp$mean_rating[is.na(tmp$mean_rating)] <- 5 #mean(tmp$mean_rating, na.rm=TRUE)
     tmp <- left_join(output, tmp)
     tmp$mean_rating[is.na(tmp$mean_rating)] <- 5
     tmp$score <- tmp$score + tmp$mean_rating
@@ -107,6 +109,74 @@ score_2 <- function(data, wgs, params=list(), districts=NULL) {
   }
   
   output %<>% arrange(desc(score))
+  
+  output %<>% 
+    select(id=wgs, score) %>% 
+    left_join(wgs) %>% 
+    select(lat, lng, score) %>% 
+    mutate(in_out = TRUE)
+  
+  return(output)
+}
+
+
+# score ver 3.0
+
+score_3 <- function(data, wgs, params=list(), districts=NULL) {
+  # data <- read_csv('data/preprocessing/places_wgs84.csv')
+  # wgs <- read_csv('data/preprocessing/warsaw_wgs84_every_500m.txt')
+  # 
+  # params = list(types_in = c('bakery'),
+  #               types_out = c('store'))
+  
+  # popular_times <- read_csv('data/preprocessing/popular_times.csv')
+  # popular_times %<>% group_by(place_id) %>% summarise(oi = mean(occupancy_index))
+  
+  data %<>% select(place_id, rating, user_ratings_total, lat, lng, type, wgs=wgs84_500_id)
+  # data %<>% left_join(popular_times)
+  
+  # data %<>% filter(!is.na(oi)) %>% filter(oi > 0)
+  
+  if (!is.null(districts)) {
+    wgs %<>% filter(district_id %in% districts)  
+  }
+  
+  types_in <- params[['types_in']]
+  types_out <- params[['types_out']]
+  
+  # score
+  output <- data %>%
+    group_by(wgs) %>%
+    summarise(score = 5)
+  for (t in types_in) {
+    tmp <- data %>% filter(type==t) %>%
+      group_by(wgs) %>% 
+      summarise(mean_rating = mean(rating, na.rm =TRUE)) %>%
+      arrange(desc(mean_rating))
+    tmp$mean_rating[is.na(tmp$mean_rating)] <- mean(tmp$mean_rating, na.rm=TRUE)
+    tmp <- left_join(output, tmp)
+    tmp$mean_rating[is.na(tmp$mean_rating)] <- 0
+    tmp$score <- tmp$score + tmp$mean_rating
+    output <- tmp %>% select(wgs, score)
+  }
+  for (t in types_out) {
+    tmp <- data %>% filter(type==t) %>%
+      group_by(wgs)
+    if (nrow(tmp) > 0){
+      tmp %<>% summarise(mean_rating = -1) %>%
+        arrange(desc(mean_rating))
+      tmp <- left_join(output, tmp)
+      tmp$mean_rating[is.na(tmp$mean_rating)] <- 0
+      tmp$score <- tmp$score + tmp$mean_rating
+      output <- tmp %>% select(wgs, score)
+    }
+  }
+  
+  output$score[output$score<0] <- 0
+  output$score <- output$score/max(output$score)*10
+  
+  output %<>% arrange(desc(score))
+  # hist(output$score)
   
   output %<>% 
     select(id=wgs, score) %>% 
