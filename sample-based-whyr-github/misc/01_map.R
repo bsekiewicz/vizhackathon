2d523e4b7a14034835e30ff382963a87821155ee
Bartosz Sękiewicz <b.sekiewicz@hta.pl>
2019-09-26 02:54:31 +0200

    moved files from WhyR Github to separate folder

--- sample-based-whyr-github/misc/01_map.R
+++ sample-based-whyr-github/misc/01_map.R
@@ -1,15 +1,19 @@
 library(magrittr)
 
 warsaw_wgs84_100 <- 
-  read.table('data/warsaw_wgs84_every_100m.txt') %>%
+  read.table('data/raw/warsaw_wgs84_every_100m.txt') %>%
   set_colnames(c('longitude', 'latitude', 'district_name'))
 
 warsaw_wgs84_500 <- 
-  read.table('data/warsaw_wgs84_every_500m.txt') %>%
+  read.table('data/raw/warsaw_wgs84_every_500m.txt') %>%
   set_colnames(c('longitude', 'latitude', 'district_n', 'district_name'))
 
 warsaw_wgs84_1000 <- 
-  read.table('data/warsaw_wgs84_every_1000m.txt') %>%
+  read.table('data/raw/warsaw_wgs84_every_1000m.txt') %>%
+  set_colnames(c('longitude', 'latitude', 'district_name'))
+
+warsaw_wgs84_2000 <- 
+  read.table('data/raw/warsaw_wgs84_every_2000m.txt') %>%
   set_colnames(c('longitude', 'latitude', 'district_name'))
 
 library(ggplot2)
@@ -23,7 +27,7 @@ ggplot(warsaw_wgs84_100, aes(x = longitude, y = latitude, col = district_name)) 
   theme_minimal() +
   theme(legend.position = 'None') +
   labs(title = 'Warsaw, every 100m') 
-ggsave('plots/warsaw_wgs84_every_100m.png')
+ggsave('sample-based-whyr-github/plots/warsaw_wgs84_every_100m.png')
 
 
 ggplot(warsaw_wgs84_500, aes(x = longitude, y = latitude, col = district_name)) +
@@ -32,7 +36,7 @@ ggplot(warsaw_wgs84_500, aes(x = longitude, y = latitude, col = district_name)) 
   theme_minimal() +
   guides(col = guide_legend(title = 'District')) +
   labs(title = 'Warsaw, every 500m') 
-ggsave('plots/warsaw_wgs84_every_500m.png')
+ggsave('sample-based-whyr-github/plots/warsaw_wgs84_every_500m.png')
 
 
 ggplot(warsaw_wgs84_1000, aes(x = longitude, y = latitude, col = district_name)) +
@@ -41,4 +45,13 @@ ggplot(warsaw_wgs84_1000, aes(x = longitude, y = latitude, col = district_name))
   theme_minimal() +
   theme(legend.position = 'None') +
   labs(title = 'Warsaw, every 1000m') 
-ggsave('plots/warsaw_wgs84_every_1000m.png')
+ggsave('sample-based-whyr-github/plots/warsaw_wgs84_every_1000m.png')
+
+
+ggplot(warsaw_wgs84_2000, aes(x = longitude, y = latitude, col = district_name)) +
+  geom_point() +
+  coord_map() + 
+  theme_minimal() +
+  theme(legend.position = 'None') +
+  labs(title = 'Warsaw, every 2000m') 
+ggsave('sample-based-whyr-github/plots/warsaw_wgs84_every_2000m.png')
